3d7e5e624ca1dbaebbaeb2b83ee4df511b334d23
Anthony <amach510@gmail.com>
2023-09-30 12:41:44 -0700

    Created Array
    added pokemonList array with name, height, and types

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-let pokemonList = [];
+let pokemonList = [
+    {name:'Bulbasaur',
+    height: 0.7,
+    types: ['grass','poison']},
+    {name:'Charmander',
+    height: 0.6,
+    types: ['fire']},
+    {name:'Squirtle',
+    height: 0.5,
+    types: ['water']},
+    {name:'Caterpie',
+    height: 0.3,
+    types: ['bug']}
+];
