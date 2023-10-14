41abb958b7edc78c8d544b633e4321ca36a9fc74
Anthony <amach510@gmail.com>
2023-10-14 15:56:33 -0700

    Created pokemonRepository IIFE and add/getAll funcitions
    commented out pokemonList array to add repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,49 @@
-let pokemonList = [
-    {name:'Bulbasaur',
-    height: 0.7,
-    types: ['grass','poison']},
-    {name:'Charmander',
-    height: 0.6,
-    types: ['fire']},
-    {name:'Squirtle',
-    height: 0.5,
-    types: ['water']},
-    {name:'Caterpie',
-    height: 0.3,
-    types: ['bug']},
-    {name:'Ninetales',
-    height: 1.1,
-    types: ['fire']}
-];
+//IIFE
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {name:'Bulbasaur', height: 0.7, types: ['grass','poison']},
+        {name:'Charmander', height: 0.6, types: ['fire']},
+        {name:'Squirtle', height: 0.5, types: ['water']},
+        {name:'Caterpie', height: 0.3, types: ['bug']},
+        {name:'Ninetales', height: 1.1, types: ['fire']}
+    ];
 
-//loop
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
+
+//Retrieve pokemonList array from IIFE
+let pokemonList = pokemonRepository.getAll();
+
+//let pokemonList = [
+   // {name:'Bulbasaur',
+   // height: 0.7,
+   // types: ['grass','poison']},
+   // {name:'Charmander',
+   // height: 0.6,
+   // types: ['fire']},
+   // {name:'Squirtle',
+   // height: 0.5,
+   // types: ['water']},
+   // {name:'Caterpie',
+   // height: 0.3,
+   // types: ['bug']},
+   // {name:'Ninetales',
+   // height: 1.1,
+   // types: ['fire']}
+//];
+
+//forEach loop
 pokemonList.forEach(function(pokemon){
     if(pokemon.height>1.0){
         document.write(pokemon.name + "(height: " + pokemon.height + "m) - Wow, that\'s big! <br>");
