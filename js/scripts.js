94047bbad9e72899cff5e1c45633aa91a137661a
Anthony <amach510@gmail.com>
2023-10-18 15:09:19 -0700

    removed comments on script.js
    removed previous comments

--- js/scripts.js
+++ js/scripts.js
@@ -27,23 +27,6 @@ let pokemonRepository = (function() {
 //Retrieve pokemonList array from IIFE
 let pokemonList = pokemonRepository.getAll();
 
-//let pokemonList = [
-   // {name:'Bulbasaur',
-   // height: 0.7,
-   // types: ['grass','poison']},
-   // {name:'Charmander',
-   // height: 0.6,
-   // types: ['fire']},
-   // {name:'Squirtle',
-   // height: 0.5,
-   // types: ['water']},
-   // {name:'Caterpie',
-   // height: 0.3,
-   // types: ['bug']},
-   // {name:'Ninetales',
-   // height: 1.1,
-   // types: ['fire']}
-//];
 
 //forEach loop
 pokemonList.forEach(function(pokemon){
@@ -53,11 +36,3 @@ pokemonList.forEach(function(pokemon){
         document.write(pokemon.name + " " + "(height: " + pokemon.height + "m) <br>");
     }
 });
-
-//commented out for loop
-    //for (let i = 0; i < pokemonList.length; i++) {
-    //    if (pokemonList[i].height > 1)
-    //        {document.write(pokemonList[i].name + "  " + "(height:" + "  " + pokemonList[i].height + "m)" + " - Wow. That\'s big!" + "<br>"); //condition for pokemon with height > 1
-    //    } else {
-    //    document.write (pokemonList[i].name + "  " + "(height:" + "  " + pokemonList[i].height + "m)" + "<br>");}
-    //}
