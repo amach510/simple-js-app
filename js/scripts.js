a528f1956a7e32ca49d435bfb1a9cebc213f803b
Anthony <amach510@gmail.com>
2023-10-26 13:51:38 -0700

    removed pokemonList array
    removed array and commented IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,6 @@
 //IIFE
 let pokemonRepository = (function() {
-    let pokemonList = [
-        {name:'Bulbasaur', height: 0.7, types: ['grass','poison']},
-        {name:'Charmander', height: 0.6, types: ['fire']},
-        {name:'Squirtle', height: 0.5, types: ['water']},
-        {name:'Caterpie', height: 0.3, types: ['bug']},
-        {name:'Ninetales', height: 1.1, types: ['fire']}
-    ];
+    let pokemonList = [];
 
     function getAll() {
         return pokemonList;
@@ -41,19 +35,6 @@ let pokemonRepository = (function() {
         showDetails: showDetails
     };
 })();
-
-//Retrieve pokemonList array from IIFE
-//let pokemonList = pokemonRepository.getAll();
-
-
-//forEach loop
-// pokemonList.forEach(function(pokemon){
-//     if(pokemon.height>1.0){
-//         document.write(pokemon.name + "(height: " + pokemon.height + "m) - Wow, that\'s big! <br>");
-//     }else{
-//         document.write(pokemon.name + " " + "(height: " + pokemon.height + "m) <br>");
-//     }
-// });
 
 pokemonRepository.getAll().forEach(function(pokemon){
     pokemonRepository.addListItem(pokemon);
